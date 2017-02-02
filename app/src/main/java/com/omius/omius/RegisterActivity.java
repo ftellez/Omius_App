14a723012001cfc41a616caf5795271217aac3d2
IngridNavarroA <ingrid.navarro.an@gmail.com>
2017-02-02 17:50:36 -0600

    Merge branch 'profile_picture' of https://github.com/ftellez/Omius_App into profile_picture
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/omius/omius/SignUpActivity.java
    #	app/src/main/res/layout/activity_signup.xml

--- app/src/main/java/com/omius/omius/RegisterActivity.java
+++ app/src/main/java/com/omius/omius/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
@@ -11,10 +12,12 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,13 +51,13 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.register_activity);
         ButterKnife.bind(this);
         photoImage = (ImageView) findViewById(R.id.photo_image);
-        photoImage.setImageDrawable(null);
+//        photoImage.setImageDrawable(null);
 
-        Button callCameraButton = (Button) findViewById(R.id.button_callcamera);
+        ImageButton callCameraButton = (ImageButton) findViewById(R.id.button_callcamera);
         callCameraButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File file = getOutputPhotoFile();
+                //File file = getOutputPhotoFile();
                 fileUri = Uri.fromFile(getOutputPhotoFile());
                 i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
                 startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQ );
@@ -68,19 +71,62 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-    }
+        /*
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+        */
 
+        /*
+          private void showPhoto(Uri photoUri) {
+            if (imageFile.exists()){
+                Drawable oldDrawable = photoImage.getDrawable();
+                if (oldDrawable != null) { ((BitmapDrawable)oldDrawable).getBitmap().recycle(); }
+                // rest as before
+            }
+        }
+        */
+    }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
     ///////// Camera methods
     private void showPhoto(Uri photoUri) {
-        File imageFile = new File(photoUri.toString());
-        if (imageFile.exists()){
-            Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-            BitmapDrawable drawable = new BitmapDrawable(this.getResources(), bitmap);
-            photoImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            photoImage.setImageDrawable(drawable);
+        File imageFile = new File(photoUri.getPath().toString());
+        if (imageFile.isFile()) {
+            Bitmap bitmap = RotateBitmap(resizeImage(imageFile.getAbsolutePath()),270);
+//            BitmapDrawable drawable = new BitmapDrawable(this.getResources(), bitmap);
+//            photoImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
+//            photoImage.setImageDrawable(drawable);
+            photoImage.setImageBitmap(bitmap);
         }
     }
+
+    private Bitmap resizeImage(String imgPath) {
+        BitmapFactory.Options options;
+        try {
+            options = new BitmapFactory.Options();
+            options.inSampleSize = 3;// 1/3 of origin image size from width and height
+            Bitmap bitmap = BitmapFactory.decodeFile(imgPath, options);
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
 
     private File getOutputPhotoFile() {
         File directory = new File(Environment.getExternalStoragePublicDirectory(
@@ -91,7 +137,8 @@ public class RegisterActivity extends AppCompatActivity {
                 return null;
             }
         }
-        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss", Locale.UK).format(new Date());
+        CharSequence charSeq = new String("yyyMMddHHmmss");
+        String timeStamp = new DateFormat().format(charSeq,new Date()).toString();
         return new File(directory.getPath() + File.separator + "IMG_"
                 + timeStamp + ".jpg");
     }
@@ -105,10 +152,12 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(this, "Image saved successfully",
                             Toast.LENGTH_LONG).show();
                     photoUri = fileUri;
+                    showPhoto(photoUri);
                 } else {
                     photoUri = data.getData();
                     Toast.makeText(this, "Image saved successfully in: " + data.getData(),
                             Toast.LENGTH_LONG).show();
+                    showPhoto(photoUri);
                 }
                 // showPhoto(photoUri);
             } else if (resultCode == RESULT_CANCELED) {
