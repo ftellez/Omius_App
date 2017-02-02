79da44ff9fa98728c9c89ef188a9356cb1dca6a5
IngridNavarroA <ingrid.navarro.an@gmail.com>
2017-02-02 15:42:05 -0600

    Created Register Activity   - Sign up and Sign In are not longer used. We should keep them for future purposes.   - Photo button not working.   - Profile picture not showing.

--- app/src/main/java/com/omius/omius/SignUpActivity.java
+++ app/src/main/java/com/omius/omius/SignUpActivity.java
@@ -38,12 +38,12 @@ public class SignUpActivity extends AppCompatActivity {
     @Bind(R.id.input_address) EditText _addressText;
     @Bind(R.id.input_email) EditText _emailText;
     @Bind(R.id.input_mobile) EditText _mobileText;
-    @Bind(R.id.input_password) EditText _passwordText;
-    @Bind(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
+    //@Bind(R.id.input_password) EditText _passwordText;
+    //@Bind(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
     @Bind(R.id.btn_signup)
     Button _signupButton;
-    @Bind(R.id.link_login)
-    TextView _loginLink;
+    //@Bind(R.id.link_login)
+    //TextView _loginLink;
 
     //Camera
    // private static final String TAG = "CallCamera";
@@ -78,6 +78,7 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+        /*
         _loginLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,6 +89,7 @@ public class SignUpActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+        */
 
         /*
           private void showPhoto(Uri photoUri) {
@@ -178,8 +180,8 @@ public class SignUpActivity extends AppCompatActivity {
         String address = _addressText.getText().toString();
         String email = _emailText.getText().toString();
         String mobile = _mobileText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String reEnterPassword = _reEnterPasswordText.getText().toString();
+        //String password = _passwordText.getText().toString();
+        //String reEnterPassword = _reEnterPasswordText.getText().toString();
 
         // TODO: Implement your own signup logic here.
 
@@ -215,8 +217,8 @@ public class SignUpActivity extends AppCompatActivity {
         String address = _addressText.getText().toString();
         String email = _emailText.getText().toString();
         String mobile = _mobileText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String reEnterPassword = _reEnterPasswordText.getText().toString();
+        //String password = _passwordText.getText().toString();
+        //String reEnterPassword = _reEnterPasswordText.getText().toString();
 
         if (name.isEmpty() || name.length() < 3) {
             _nameText.setError("at least 3 characters");
@@ -247,21 +249,21 @@ public class SignUpActivity extends AppCompatActivity {
             _mobileText.setError(null);
         }
 
+        /*
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
             _passwordText.setError("between 4 and 10 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
         }
-
         if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
             _reEnterPasswordText.setError("Password Do not match");
             valid = false;
         } else {
             _reEnterPasswordText.setError(null);
         }
+        */
 
         return valid;
     }
-
 }
